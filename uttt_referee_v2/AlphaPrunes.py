00f136ac2b144bb1d887eaa8006af8094a33fb0f
Erin Carter <eringcarter@gmail.com>
2022-09-23 14:15:47 -0400

    Edited variable issues

--- uttt_referee_v2/AlphaPrunes.py
+++ uttt_referee_v2/AlphaPrunes.py
@@ -17,7 +17,7 @@ def readMoves(file):
     f = open(file, "r")
     lines = file.readlines()
     for line in lines:
-        last_move = line
+        lastMove = line
         if line.isspace():
             break
         else:
@@ -27,15 +27,15 @@ def readMoves(file):
                 board[int(moves[1]),int(moves[2])] = 1 # X = 1
             else:
                 board[int(moves[1]), int(moves[2])] = 2 # O = 2
-    return last_move
+    return lastMove
 
 def findNextMove():
     lastMove = readMoves("move.txt")
     takenList = []
     for i in range(0,8):
-        if board[lastmove[2]][i] = 1 or board[lastMove[2]][i] = 2:
-            takenList.append(board[lastmove[2]][i])
-    move = [lastmove[2],random.choice([i for i in range(0,8) if i not in takenList])]
+        if board[lastMove[2]][i] == 1 or board[lastMove[2]][i] == 2:
+            takenList.append(board[lastMove[2]][i])
+    move = [lastMove[2],random.choice([i for i in range(0,8) if i not in takenList])]
     board[move[0]][move[1]] = 1
     return move
 def addMove():
