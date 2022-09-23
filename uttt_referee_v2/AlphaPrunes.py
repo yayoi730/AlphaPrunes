707fa735ed51e68bc3dcebfa92e023a792c90412
bfgen <bfgenin@wpi.edu>
2022-09-23 14:26:17 -0400

    main

--- uttt_referee_v2/AlphaPrunes.py
+++ uttt_referee_v2/AlphaPrunes.py
@@ -5,14 +5,18 @@ import pygame
 
 board = np.zeros((9, 9))
 
-def initBoard():
+
+def main():
     readMoves('first_four_moves.txt')
+    
 
 
 def makeAMove():
     readMoves()
     findNextMove()
     addMove()
+
+
 def readMoves(file):
     f = open(file, "r")
     lines = file.readlines()
@@ -24,22 +28,27 @@ def readMoves(file):
             # populates matrices
             moves = line.split()
             if moves[0] == "X":
-                board[int(moves[1]),int(moves[2])] = 1 # X = 1
+                board[int(moves[1]), int(moves[2])] = 1  # X = 1
             else:
-                board[int(moves[1]), int(moves[2])] = 2 # O = 2
+                board[int(moves[1]), int(moves[2])] = 2  # O = 2
     return lastMove
+
 
 def findNextMove():
     lastMove = readMoves("move.txt")
     takenList = []
-    for i in range(0,8):
+    for i in range(0, 8):
         if board[lastMove[2]][i] == 1 or board[lastMove[2]][i] == 2:
             takenList.append(board[lastMove[2]][i])
-    move = [lastMove[2],random.choice([i for i in range(0,8) if i not in takenList])]
+    move = [lastMove[2], random.choice([i for i in range(0, 8) if i not in takenList])]
     return move
+
 
 def addMove(globalBoard, localBoard):
     board[globalBoard][localBoard] = 1
     f = open("move_file.txt", "a")
     f.write("X " + globalBoard + " " + localBoard)
     f.close()
+
+if __name__ == "__main__":
+    main()
