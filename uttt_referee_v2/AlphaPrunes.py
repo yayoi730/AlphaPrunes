36c9917540ede01ec4f6d44fef3e94b97c47ad72
Erin Carter <eringcarter@gmail.com>
2022-09-23 14:08:43 -0400

    Added findNextMove Code

--- uttt_referee_v2/AlphaPrunes.py
+++ uttt_referee_v2/AlphaPrunes.py
@@ -1,4 +1,6 @@
 import numpy as np
+from random import random
+
 import pygame
 
 board = np.zeros((9, 9))
@@ -15,5 +17,12 @@ def readMoves():
     f = open("move.txt", "r")
     lines = f.readlines()
 def findNextMove():
-
+    lastMove = readMoves("move.txt")
+    takenList = []
+    for i in range(0,8):
+        if board[lastmove[2]][i] = 1 or board[lastMove[2]][i] = 2:
+            takenList.append(board[lastmove[2]][i])
+    move = [lastmove[2],random.choice([i for i in range(0,8) if i not in takenList])]
+    board[move[0]][move[1]] = 1
+    return move
 def addMove():
