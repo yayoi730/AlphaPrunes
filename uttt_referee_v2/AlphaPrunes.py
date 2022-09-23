a46d2f7ba5bc3625de88a1fb653efcc4901901d5
Erin Carter <eringcarter@gmail.com>
2022-09-23 13:12:43 -0400

    Added potential AI functions

---
+++ uttt_referee_v2/AlphaPrunes.py
@@ -0,0 +1,12 @@
+import pygame
+
+def makeAMove():
+    readMoves()
+    findNextMove()
+    addMove()
+def readMoves():
+    f = open("move.txt", "r")
+    lines = f.readlines()
+def findNextMove():
+
+def addMove():
