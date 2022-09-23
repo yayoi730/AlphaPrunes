03a6026799f11bb391fa4f93f3c3d388b0e91a31
bfgen <bfgenin@wpi.edu>
2022-09-23 15:48:32 -0400

    index fix

--- uttt_referee_v2/AlphaPrunes.py
+++ uttt_referee_v2/AlphaPrunes.py
@@ -2,7 +2,7 @@ import os
 from os.path import exists
 
 import numpy as np
-from random import random
+import random
 
 import pygame
 
@@ -15,7 +15,7 @@ def main():
     print("here1")
     print(os.listdir())
     move = findNextMove(readMoves('first_four_moves'))
-    addMove(move[0], move[1])
+    addMove(int(move[0]), int(move[1]))
 
 
 def readMoves(file):
@@ -29,25 +29,28 @@ def readMoves(file):
             # populates matrices
             moves = line.split()
             if moves[0] == "X":
-                board[int(moves[1]), int(moves[2])] = 1  # X = 1
+                board[int(moves[1])][int(moves[2])] = 1  # X = 1
             else:
-                board[int(moves[1]), int(moves[2])] = 2  # O = 2
+                board[int(moves[1])][int(moves[2])] = 2  # O = 2
     f.close()
     return lastMove
 
 
 def findNextMove(lastMove):
     print(board)
+    lastMove = int(lastMove[2])
     takenList = []
     for i in range(0, 8):
-        if board[int(lastMove[2])][i] == 1 or board[int(lastMove[2])][i] == 2:
-            takenList.append(board[lastMove[2]][i])
-    move = [int(lastMove[2]), random.choice([i for i in range(0, 8) if i not in takenList])]
+        if board[lastMove][i] == 1 or board[lastMove][i] == 2:
+            takenList.append(board[lastMove][i])
+    move = [lastMove, random.choice([i for i in range(0, 8) if i not in takenList])]
+    print(move)   
     return move
 
 
 def addMove(globalBoard, localBoard):
     board[globalBoard][localBoard] = 1
+    print(board)
     f = open("move_file", "a")
     f.write("X " + globalBoard + " " + localBoard)
     f.close()
